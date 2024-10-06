91097f39de746399fa2e884dc87da4bdac19d759
jessicaden <jessica.adeniran@laetottenham.ac.uk>
2024-10-06 15:00:13 +0000

    Pending changes exported from your codespace

--- Task1.py
+++ Task1.py
@@ -7,40 +7,47 @@
 
 ###########################################
 
-def weird_calculation():
-    # get the length and width of the first triangle from the user
-    opp1 = float(input("Enter your first triangle's opposite side length: "))
-    adj1 = float(input("Enter your first triangle's adjacent side length: "))
-
-    # work out the hyp
+def weird_calculation(opp1,adj1):
     import math
     hyp1 = math.sqrt(opp1**2 + adj1**2)
-
-    # get the length and width of the second triangle from the user
-    opp2 = float(input("Enter your second triangle's opposite side length: "))
-    adj2 = float(input("Enter your second triangle's adjacent side length: "))
-
-    # work out the hyp
+    return hyp1
+def weird_calculation1(opp2,adj2):
     import math
     hyp2 = math.sqrt(opp2**2 + adj2**2)
-
-    # create a third triangle with the hyp1 as the opp and hyp2 as the adj
-    opp3 = hyp1
-    adj3 = hyp2
-    
+    return hyp2
+def weird_calculation2(hyp1,hyp2):
     import math
-    hyp3 = math.sqrt(opp3**2 + adj3**2)
+    hyp3 = math.sqrt(hyp1**2 + hyp2**2)
     return hyp3
 
-weird_answer = weird_calculation()
-print(weird_answer)
+while True:
+    opp1 = float(input("Enter your first triangle's opposite side length: "))
+    adj1 = float(input("Enter your first triangle's adjacent side length: "))
+    if opp1>0 and adj1>0:
+        break
+    print("values entered should be more than 0")
+
+while True:
+    opp2 = float(input("Enter your second triangle's opposite side length: "))
+    adj2 = float(input("Enter your second triangle's adjacent side length: "))
+    if opp2>0 and adj2>0:
+        break
+    print("values entered should be more than 0")
+
+tri1=weird_calculation(opp1,adj1)
+tri2=weird_calculation1(opp2,adj2)
+result=weird_calculation2(tri1,tri2)
+print(result)
 
 
 # After you have written the reusable functions, answer the following:
 # Questions:
 # 1. What are the preconditions for your code not to break?
+# - there has to be two numbers taken as parameters
+# - the numbers have to be more than 0
 # 2. Validate the user's input based on your preconditions.
 # 3. Why was it useful to use reusable components in this case? Please mention at least 2 reasons and don't forget to contextualise.
+# - makes code shorter and easier to edit if needed for use in a different scenario such as using same formula to calculate triangles with different lengths
 
 # Further Tasks:
 # 1. Put your functions in seperate appropriate files and import them in.
--- Task2.py
+++ Task2.py
@@ -6,6 +6,10 @@ import os, time
 
 # Define your reusable functions here:
 # Make sure each function only does ONE thing!!!!!!!!!!!
+f = Figlet(font="slant")
+print(f.renderText("HEY!"))
+print("Welcome to the weird weather bot :)")
+print("-----------------------------------\n")
 
 def guess_gender(name):
     gender_resp = requests.get(f"https://api.genderize.io/?name={name}").json()
@@ -13,80 +17,87 @@ def guess_gender(name):
     prob_percent = gender_resp["probability"] * 100
     return [gender, prob_percent]
 
-###########################################
-
-def weird_weather_bot():
-    
-    f = Figlet(font="slant")
-    print(f.renderText("HEY!"))
-
-    print("Welcome to the weird weather bot :)")
-    print("-----------------------------------\n")
-    name = input("May I take your first name please? ")
-    gender_result = guess_gender(name)
-    gender = gender_result[0]
-    prob_percent = gender_result[1]
-    print(f"\nHmmm, I'm {prob_percent}% sure you are a {gender}.")
-    
-    gender_correct = input("Am I right? :) (Y/n)")
-    if gender_correct.lower() in ["", "yes", "y", "ye"]:
-        print("Wooooooh! Computer 1, Human 0.")
-    else:
-        print("Ahhhh, sorry! :(")
-
-    postcode_raw = input("\nSo, what's your postcode? ")
-    postcode_resp = requests.get(f"https://api.postcodes.io/postcodes/{postcode_raw}").json()
+def weird_weather_bot(postcode):
+    postcode_resp = requests.get(f"https://api.postcodes.io/postcodes/{postcode}").json()
 
     area = postcode_resp['result']['admin_ward']
     longitude = postcode_resp['result']['longitude']
     latitude = postcode_resp['result']['latitude']
-    print(f"Nice! so you live in {area}.\n")
+    return [area, longitude, latitude]
 
-    print("Let me just check the weather there today...\n")
-    
-    for i in range(3):
-        time.sleep(1)
-        print("...")
-    
-    input("\nWould you like a cat fact while you wait? ")
-    print("Doesn't matter what you think, I'm going to give you one anyway :)")
-    time.sleep(3)
+def catfactbot():
     joke_resp = requests.get("https://catfact.ninja/fact").json()
     joke = joke_resp['fact']
-    print("\n###########################")
-    print("CAT FACT:")
-    print(f"\n{joke}\n")
-    print("So interesting isn't it!")
-    print("###########################")
+    return joke
 
-    print("\nWaiting 5 seconds for you to read the fact...")
-    time.sleep(5)
-    print("\nNow, back to getting the weather...")
-
-    for i in range(3):
-        time.sleep(1)
-        print("...")
-
+def weather_results():
     weather_resp = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid=4d30afa58f6f935d861edecad3639cda").json()
-    # print(weather_resp)
     weather_kelvin = weather_resp["main"]["temp"]
-    # convert to degrees
     weather_degrees = int(weather_kelvin - 273.15)
     main_weather_desc = weather_resp["weather"][0]["main"]
     second_weather_desc = weather_resp["weather"][0]["description"]
-    print(f"\nThe weather in {area}:\n")
-    print(str(weather_degrees) + "℃")
-    print(f"{main_weather_desc} - {second_weather_desc}")
-    print("\nThank you! Bye.")
+    return [weather_degrees,main_weather_desc,second_weather_desc]
 
-weird_weather_bot()
+name = input("May I take your first name please? ")
+gender_result = guess_gender(name)
+gender = gender_result[0]
+prob_percent = gender_result[1]
+print(f"\nHmmm, I'm {prob_percent}% sure you are a {gender}.")
 
+gender_correct = input("Am I right? :) (Y/n)")
+if gender_correct.lower() in ["", "yes", "y", "ye"]:
+    print("Wooooooh! Computer 1, Human 0.")
+else:
+    print("Ahhhh, sorry! :(")
+###########################################
+postcode=input("\nSo, what's your postcode? ")
+post=weird_weather_bot(postcode)
+area=post[0]
+latitude=post[1]
+longitude=post[2]
+print(f"Nice! so you live in",area+".\n")
+
+print("Let me just check the weather there today...\n")
+    
+for i in range(3):
+    time.sleep(1)
+    print("...")
+###########################################
+input("\nWould you like a cat fact while you wait? ")
+print("Doesn't matter what you think, I'm going to give you one anyway :)")
+time.sleep(3)
+print("\n###########################")
+print("CAT FACT:")
+jokeres=catfactbot()
+print("\n"+jokeres,"\n")
+print("So interesting isn't it!")
+print("###########################")
+
+print("\nWaiting 5 seconds for you to read the fact...")
+time.sleep(5)
+print("\nNow, back to getting the weather...")
+
+for i in range(3):
+    time.sleep(1)
+    print("...")
+
+
+res=weather_results()
+degs=res[0]
+main=res[1]
+second=res[2]
+print(f"\nThe weather in",area,":\n")
+print(str(degs) + "℃")
+print(f"{main} - {second}")
+print("\nThank you! Bye.")
 
 # After you have written the reusable functions, answer the following:
 # Questions:
 # 1. What are the preconditions for your code not to break?
+# - the name has to only be letters, degrees have to be an integer, postcode input has to be an actual postcode
 # 2. Validate the user's input based on your preconditions.
 # 3. Why was it useful to use reusable components in this case? Please mention at least 2 reasons and don't forget to contextualise.
+# - to make the code cleaner and easier to understand for which part of the code is being ran for each function, such as the first function being for gender guessing
 
 # Further Tasks:
 # 1. Put your functions in seperate appropriate files and import them in.
